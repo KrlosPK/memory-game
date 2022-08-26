dee17a0f3e5f1b2df1682fa6576a0d8a2ea22b0e
KrlosPK <centille095@gmail.com>
2022-08-25 23:29:12 -0500

    Proyecto finalizado (futuros nuevos niveles)

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ let tarjeta1 = null;
 let tarjeta2 = null;
 let primerResultado = null;
 let segundoResultado = null;
+let movimientos = 0;
+let aciertos = 0;
+let temporizador = false;
+let tiempoInicial = 30;
+let tiempo = 30;
+let tiempoRegresivoId = null;
+
+// DOM
+let movimientosEl = document.getElementById("movimientos");
+let aciertosEl = document.getElementById("aciertos");
+let tiempoEl = document.getElementById("tiempoRestante");
+let replayEl = document.getElementById("jugarDeNuevo");
 
 // Generación de números aleatorios
 let numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
@@ -14,7 +26,37 @@ numbers.sort(() => {
 });
 console.log(numbers);
 
+// Funciones
+function contarTiempo() {
+    tiempoRegresivoId = setInterval(() => {
+        tiempo--;
+        tiempoEl.innerHTML = tiempo + " segundos";
+        if (tiempo == 0) {
+            clearInterval(tiempoRegresivoId);
+            gameOver();
+        }
+    }, 1000);
+}
+function gameOver() {
+    for (let i = 0; i <= 15; i++) {
+        let tarjetaBloqueada = document.getElementById(i);
+        tarjetaBloqueada.innerHTML = numbers[i];
+        tarjetaBloqueada.classList.add("disabled");
+    }
+    Swal.fire({
+        icon: "error",
+        title: "Perdiste...",
+        text: "Inténtalo de nuevo!",
+    });
+    replayEl.classList.remove("hidden");
+}
+
 function voltear(id) {
+    if (temporizador == false) {
+        contarTiempo();
+        temporizador = true;
+    }
+
     tarjetasVolteadas++;
     console.log(tarjetasVolteadas);
     if (tarjetasVolteadas == 1) {
@@ -25,5 +67,49 @@ function voltear(id) {
 
         // Deshabilitar primer botón
         tarjeta1.classList.add("disabled");
+    } else if (tarjetasVolteadas == 2) {
+        // Mostrar segundo número
+        tarjeta2 = document.getElementById(id);
+        segundoResultado = numbers[id];
+        tarjeta2.innerHTML = segundoResultado;
+
+        // Deshabilitar segundo botón
+        tarjeta2.classList.add("disabled");
+
+        // Incrementar movimientos
+        movimientos++;
+        movimientosEl.innerHTML = movimientos;
+        if (primerResultado === segundoResultado) {
+            // Encerar contador tarjetas destapadas
+            tarjetasVolteadas = 0;
+
+            // Aumentar los aciertos
+            aciertos++;
+            aciertosEl.innerHTML = aciertos;
+
+            if (aciertos == 8) {
+                clearInterval(tiempoRegresivoId);
+                aciertosEl.innerHTML = `${aciertos} 😱`;
+                tiempoEl.innerHTML = `Fantástico, solo te demoraste ${
+                    tiempoInicial - tiempo
+                } segundos 🎉`;
+                movimientosEl.innerHTML = `${movimientos} 😎`;
+                Swal.fire({
+                    icon: "success",
+                    title: "¡Enhorabuena!",
+                    text: "Haz ganado 🥳",
+                });
+                replayEl.classList.remove("hidden");
+            }
+        } else {
+            // Mostrar momentáneamente
+            setTimeout(() => {
+                tarjeta1.innerHTML = " ";
+                tarjeta2.innerHTML = " ";
+                tarjeta1.classList.remove("disabled");
+                tarjeta2.classList.remove("disabled");
+                tarjetasVolteadas = 0;
+            }, 800);
+        }
     }
 }
